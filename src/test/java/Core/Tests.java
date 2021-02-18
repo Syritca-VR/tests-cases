8f12e42303506c25a05154128b8d49488f2174f7
Viacheslav <siriza-vr@yandex.ru>
2021-02-18 14:18:16 +0300

    fixed tests added new locators

--- src/test/java/Core/Tests.java
+++ src/test/java/Core/Tests.java
@@ -9,7 +9,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -35,16 +34,12 @@ public class Tests {
     public void test1() throws InterruptedException {
         Common.getUrl(driver,"https://www.google.com/");
         String googleUrl = driver.getCurrentUrl();
-        Thread.sleep(2000);
         assert googleUrl.equals("https://www.google.com/");
         Common.inputValue(driver, By.cssSelector(html_selector.getProperty("google_input_text")), "яндекс маркет");
-        Thread.sleep(3000);
         Common.clickWhenVisible(driver, By.cssSelector(html_selector.getProperty("search_in_google_btn")));
-        Thread.sleep(3000);
         String marketLink = driver.findElement(By.xpath(html_xpath.getProperty("yandex_market_link_in_google"))).getText();
         assert marketLink.startsWith("Яндекс.Маркет");
         driver.findElement(By.xpath(html_xpath.getProperty("yandex_market_link_in_google"))).click();
-        Thread.sleep(3000);
         String marketUrl = driver.getCurrentUrl();
         assert marketUrl.equals("https://market.yandex.ru/");
     }
@@ -52,10 +47,8 @@ public class Tests {
     @Test(priority = 2, testName = "searchGoodVacuumCleaner")
     public void test2() throws InterruptedException {
         driver.get("https://market.yandex.ru/");
-        Thread.sleep(4000);
         String marketUrl = driver.getCurrentUrl();
         assert marketUrl.equals("https://market.yandex.ru/");
-        Thread.sleep(3000);
         Common.inputValue(driver, By.xpath(html_xpath.getProperty("search_on_yandex_market")), "пылесосы");
         Common.clickWhenVisible(driver, By.cssSelector(html_selector.getProperty("yandex_market_search_button")));
         Common.clickWhenVisible(driver, By.xpath(html_xpath.getProperty("yandex_market_vacuum_cleaner_button")));
@@ -68,16 +61,11 @@ public class Tests {
         Common.inputValue(driver, By.xpath(html_xpath.getProperty("search_brand")), "polaris");
         Common.clickWhenVisible(driver, By.xpath(html_xpath.getProperty("polaris_vacuum_cleaner_button")));
         Common.inputValue(driver, By.xpath(html_xpath.getProperty("yandex_market_set_price")), "6001");
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//*[@id=\"search-prepack\"]/div/div[3]/div/div/div[2]/div[2]/div/div/fieldset/div/ul"));
-        Thread.sleep(3000);
-        WebElement showAll = driver.findElement(By.xpath("//*[@id=\"search-prepack\"]/div/div[3]/div/div/div[3]/div/div/a/span"));
+        driver.findElement(By.xpath(html_xpath.getProperty("all_filters")));
+        WebElement showAll = driver.findElement(By.xpath(html_xpath.getProperty("all_filters")));
         scroll.moveToElement(showAll).release().perform();
         showAll.click();
-        Thread.sleep(3000);
-        Common.clickWhenVisible(driver, By.xpath("/html/body/div[2]/section/div[2]/div/div/div[3]/div/div/a[2]"));
-        Thread.sleep(3000);
-        scroll.moveToElement(driver.findElement(By.xpath("//*[@id=\"search-prepack\"]/div/div[3]/div/div/div[2]/div[4]/div/div/fieldset/div/label/div"))).release().perform();
-        Thread.sleep(5000);
+        Common.clickWhenVisible(driver, By.xpath(html_xpath.getProperty("show_all_button")));
+        scroll.moveToElement(driver.findElement(By.xpath(html_xpath.getProperty("yandex_market_only_in_sale_button")))).release().perform();
     }
 }
